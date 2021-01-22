7d539accffcd07570232d8ccc91a19970472d9d2
Daniel <danielkarpen97@gmail.com>
2021-01-22 10:53:23 -0600

    assign character count

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 import dom from "./lib.js";
 
 dom.textarea.addEventListener("keyup", function handleTextarea() {
+  const charCount = this.value.length;
   if (this.value.length) {
     dom.button.disabled = false;
   } else {
     dom.button.disabled = true;
   }
+
+  dom.charCountSpan.innerText = charCount;
 });
