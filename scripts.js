f7b10722d4708578cfeec09b62fc463c5cf77031
Daniel <danielkarpen97@gmail.com>
2021-01-22 11:26:22 -0600

    modular code edit

--- scripts.js
+++ scripts.js
@@ -1,18 +1,9 @@
-import dom from "./lib.js";
+import { dom, toggleBtn, toggleCharCountError } from "./lib";
 
 dom.textarea.addEventListener("keyup", function handleTextarea() {
   const charCount = this.value.length;
-  if (this.value.length) {
-    dom.button.disabled = false;
-  } else {
-    dom.button.disabled = true;
-  }
 
   dom.charCountSpan.innerText = charCount;
-
-  if (charCount > 140) {
-    dom.span.classList.add("is-error");
-  } else {
-    dom.span.classList.remove("is-error");
-  }
+  toggleBtn(charCount);
+  toggleCharCountError(charCount);
 });
