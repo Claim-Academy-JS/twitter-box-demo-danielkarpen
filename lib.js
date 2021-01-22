5c37ce04accd20bf0cf04d7b60334a7bcc0c76a4
Daniel <danielkarpen97@gmail.com>
2021-01-22 10:57:47 -0600

    added is-error class css and script functionality

--- lib.js
+++ lib.js
@@ -4,5 +4,6 @@ const span = form.querySelector("span");
 export default {
   button: form.querySelector("button"),
   charCount: span.querySelector("span"),
+  span,
   textarea: form.querySelector("textarea"),
 };
